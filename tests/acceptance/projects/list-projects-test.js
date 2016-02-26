d246ac6b3cff6a6670f8b5ec47fdf59eef65633d
R.S. Schermer <roland0507@gmail.com>
2016-02-26 15:31:56 +0100

    Add initial (failing) acceptence test for listing projects

---
+++ tests/acceptance/projects/list-projects-test.js
@@ -0,0 +1,38 @@
+/* jshint expr:true */
+import {
+  describe,
+  it,
+  beforeEach,
+  afterEach
+} from 'mocha';
+import { expect } from 'chai';
+import startApp from '../../helpers/start-app';
+import destroyApp from '../../helpers/destroy-app';
+
+describe('Acceptance: Creating a project', function () {
+  let application;
+
+  beforeEach(function () {
+    application = startApp();
+  });
+
+  afterEach(function () {
+    destroyApp(application);
+  });
+
+  describe('3 projects exist', function () {
+    beforeEach(function () {
+      server.createList('project', 3);
+    });
+
+    describe('I visit the page for listing projects', function () {
+      beforeEach(function () {
+        visit('projects');
+      });
+
+      it('lists 3 projects', function () {
+        expect(find('.projects-table tr.project-row').length).to.equal(3);
+      });
+    });
+  });
+});
