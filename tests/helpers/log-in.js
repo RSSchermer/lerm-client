61af4f1193b681da2d8083030e48a69a69a9f5e9
R.S. Schermer <roland0507@gmail.com>
2016-02-28 20:53:18 +0100

    Fix logIn helper by passing authenticateSession a proper access token

--- tests/helpers/log-in.js
+++ tests/helpers/log-in.js
@@ -1,12 +1,21 @@
 import Ember from 'ember';
 import { authenticateSession } from 'lerm-client/tests/helpers/ember-simple-auth';
 
-const { Test, run } = Ember;
+const { Test } = Ember;
 
 export default Test.registerAsyncHelper('logIn',
   function(app, user) {
     user = user || server.create('user');
-    server.create('accessToken', {resourceOwnerId: user.id});
-    run(() => authenticateSession(app));
+
+    const accessToken = server.create('accessToken', {resourceOwnerId: user.id});
+
+    authenticateSession(app, {
+      access_token: accessToken.accessToken,
+      token_type: accessToken.tokenType,
+      expires_in: accessToken.expiresIn,
+      created_at: accessToken.createdAt
+    });
+
+    wait();
   }
 );
