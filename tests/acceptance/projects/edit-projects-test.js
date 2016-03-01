8ca19748a896b589c253de5f0c0bb024ecbe6708
R.S. Schermer <roland0507@gmail.com>
2016-03-01 16:20:58 +0100

    Setup acceptence test for editing a project

---
+++ tests/acceptance/projects/edit-projects-test.js
@@ -0,0 +1,145 @@
+/* jshint expr:true */
+import {
+  describe,
+  it,
+  beforeEach,
+  afterEach
+} from 'mocha';
+import { expect } from 'chai';
+import startApp from '../../helpers/start-app';
+import destroyApp from '../../helpers/destroy-app';
+
+describe('Acceptance - Projects: Editing a project', function() {
+  let application;
+
+  beforeEach(function() {
+    application = startApp();
+  });
+
+  afterEach(function() {
+    destroyApp(application);
+  });
+
+  describe('A project exists', function() {
+    beforeEach(function() {
+      this.project = server.create('project', { name: 'Some project' });
+    });
+
+    describe('I am not logged in', function() {
+      describe('I visit the page for the project', function() {
+        beforeEach(function() {
+          visit(`projects/${this.project.id}`);
+        });
+
+        it('does not show a link for creating a new project', function () {
+          expect(find('.edit-link')).to.be.empty;
+        });
+
+        describe('I visit the page for editing the project', function() {
+          beforeEach(function() {
+            visit(`projects/${this.project.id}/edit`);
+          });
+
+          it('redirects me to the login page', function() {
+            expect(currentPath()).to.equal('login');
+          });
+        });
+      });
+    });
+
+    describe('I am logged as a user who is not a project member', function() {
+      beforeEach(function() {
+        logIn();
+      });
+
+      describe('I visit the page for the project', function() {
+        beforeEach(function() {
+          visit(`projects/${this.project.id}`);
+        });
+
+        it('does not show a link for creating a new project', function () {
+          expect(find('.edit-link')).to.be.empty;
+        });
+
+        describe('I visit the page for editing the project', function() {
+          beforeEach(function() {
+            visit(`projects/${this.project.id}/edit`);
+          });
+
+          it('tells me I am not authorized to open this page', function() {
+            expect(find('main').text()).to.contain('Unauthorized');
+          });
+        });
+      });
+    });
+
+    describe('I am logged as a user who is a project member', function() {
+      beforeEach(function() {
+        this.currentUser = server.create('user');
+        server.create('membership', { user: this.currentUser, project: this.project });
+        logIn(this.currentUser);
+      });
+
+      describe('I visit the page for the project', function() {
+        beforeEach(function() {
+          visit(`projects/${this.project.id}`);
+        });
+
+        it('shows a link for creating a new project', function () {
+          expect(find('.edit-link')).to.not.be.empty;
+        });
+
+        describe('I visit the page for editing the project', function() {
+          beforeEach(function() {
+            visit(`projects/${this.project.id}/edit`);
+          });
+
+          it('shows a form for editing the project', function() {
+            expect(find('.project-edit-form')).to.not.be.empty;
+          });
+
+          describe('I fill out the form with valid project data', function() {
+            beforeEach(function() {
+              fillIn('#name', 'New project name');
+              fillIn('#description', 'Some description');
+            });
+
+            describe('I leave the page for creating projects without submitting the form', function() {
+              beforeEach(function() {
+                visit('/');
+              });
+
+              describe('I visit the page for the project', function() {
+                beforeEach(function() {
+                  visit(`projects/${this.project.id}`);
+                });
+
+                it('shows the old name for the project', function() {
+                  expect(find('main').text()).to.contain('Some project');
+                });
+              });
+            });
+
+            describe('I submit the form', function() {
+              beforeEach(function() {
+                click('.project-edit-form .btn-primary');
+              });
+
+              it('transitions to the projects\'s overview page', function() {
+                expect(currentPath()).to.equal('projects.show');
+              });
+
+              it('displays a flash message indicating that the new project was updated successfully', function() {
+                expect(find('main').text()).to.contain('The project was updated successfully');
+              });
+
+              it('shows the updated name for the project', function() {
+                expect(find('main').text()).to.contain('New project name');
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
