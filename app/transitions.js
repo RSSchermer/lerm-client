0de6e2581569cfaca33c6a6a299b922c2764651a
R.S. Schermer <roland0507@gmail.com>
2016-03-07 12:07:38 +0100

    Add animated transitions for adding members and data elements

--- app/transitions.js
+++ app/transitions.js
@@ -19,4 +19,18 @@ export default function() {
     this.use('toLeft'),
     this.reverse('toRight')
   );
+
+  this.transition(
+    this.fromRoute('projects.show.members.index'),
+    this.toRoute('projects.show.members.new'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('projects.show.data-elements.index'),
+    this.toRoute('projects.show.data-elements.new'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
 }
