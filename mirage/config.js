42685f5b6487c6e981b0f7bcb1930cecae91f7c0
R.S. Schermer <roland0507@gmail.com>
2016-03-07 15:00:56 +0100

    Clean up mirage server config

--- mirage/config.js
+++ mirage/config.js
@@ -9,23 +9,20 @@ export default function() {
   this.namespace = 'oauth';
 
   this.post('/token', (schema, request) => {
-    const params = qs.parse(request.requestBody);
+    let params = qs.parse(request.requestBody);
 
     if (params.grant_type === 'password' &&
         params.password === 'valid_password') {
-      const resourceOwner = schema.user.create({
-        email: params.username,
+      let resourceOwner = schema.db.users.firstOrCreate({ email: params.username }, {
         username: 'current_user',
         firstName: 'Current',
         lastName: 'User'
       });
 
-      const token = schema.accessToken.create({
+      let token = schema.accessToken.create({
         accessToken: '824228db73ee5cc495af0716c644374b308c94dc71dff13ed3d8419a7bca1eeb',
         tokenType: 'bearer',
         resourceOwnerId: resourceOwner.id,
-        scopes: [],
-        application: {uid: null},
         expiresIn: 7200,
         createdAt: Math.floor(Date.now() / 1000)
       });
@@ -43,11 +40,10 @@ export default function() {
 
   // Returns information for the most recently created token
   this.get('/token/info', (schema) => {
-    // TODO: make pull request on ember mirage to add a last() method
-    const accessTokens = schema.accessToken.all();
+    let accessTokens = schema.accessToken.all();
 
     if (accessTokens.length > 0) {
-      const accessToken = accessTokens[accessTokens.length - 1];
+      let accessToken = accessTokens[accessTokens.length - 1];
 
       return {
         'resource_owner_id': accessToken.resourceOwnerId,
@@ -65,9 +61,9 @@ export default function() {
 
   this.namespace = 'api/v1';
 
-  this.get('/users', function(db, request) {
+  this.get('/users', function(schema, request) {
     // TODO: remove workaround when ember-cli-mirage support the filter param out of the box
-    let users = db.user.all();
+    let users = schema.user.all();
     let emailFilter = request.queryParams['filter[email]'];
 
     if (emailFilter) {
