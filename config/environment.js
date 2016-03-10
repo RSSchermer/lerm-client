0fe740b3cba6fe88502b1f11ce9db6f19960b28f
R.S. Schermer <roland0507@gmail.com>
2016-03-11 00:08:24 +0100

    Refactor to partial pods structure (all routes, template, controller except top level)

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'lerm-client',
+    podModulePrefix: 'lerm-client/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
