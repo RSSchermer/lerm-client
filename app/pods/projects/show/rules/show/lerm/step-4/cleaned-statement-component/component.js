8dea4e5eb83ccb350eae97730825213c1f9b92ef
R.S. Schermer <roland0507@gmail.com>
2016-03-21 13:28:37 +0100

    Clean up cleaned statement component implementation

--- app/pods/projects/show/rules/show/lerm/step-4/cleaned-statement-component/component.js
+++ app/pods/projects/show/rules/show/lerm/step-4/cleaned-statement-component/component.js
@@ -2,19 +2,18 @@ import Ember from 'ember';
 
 const { Component, computed } = Ember;
 
+const { alias } = computed;
+
 export default Component.extend({
   isEditing: false,
 
-  cleanedPhraseTexts: computed(
-    'statement.rule.phrases.@each.originalText',
-    'statement.rule.phrases.@each.cleanedText',
-    'statement.rule.phrases.@each.discarded',
-    function() {
-      return this.get('statement.rule.phrases')
-        .filter((p) => !p.get('discarded'))
-        .map((p) => p.get('cleanedText') || p.get('originalText'));
-    }
-  ),
+  rule: alias('statement.rule'),
+
+  phrases: alias('rule.phrases'),
+
+  cleanedPhraseTexts: computed('phrases.@each.text', 'phrases.@each.discarded', function() {
+    return this.get('phrases').filter((p) => !p.get('discarded')).map((p) => p.get('text'));
+  }),
 
   actions: {
     startEditing() {
