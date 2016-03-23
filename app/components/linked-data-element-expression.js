5c628108b55e5af6462e14017a0d7d20125ba6f6
R.S. Schermer <roland0507@gmail.com>
2016-03-23 16:42:24 +0100

    Add component for creating links to the data elements used in an expression

---
+++ app/components/linked-data-element-expression.js
@@ -0,0 +1,49 @@
+import Ember from 'ember';
+
+const { Component, computed, inject, observer, $ } = Ember;
+const { sort } = computed;
+
+export default Component.extend({
+  routing: inject.service('-routing'),
+
+  sortedDataElements: sort('dataElements', (a, b) => {
+    if (a.get('label.length') > b.get('label.length')) {
+      return -1;
+    } else if (a.get('label.length') < b.get('label.length')) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }),
+
+  didInsertElement() {
+    this.renderLinkedExpression();
+  },
+
+  maintainExpression: observer('sortedDataElements.@each.label', 'expression', function() {
+    this.renderLinkedExpression();
+  }),
+
+  renderLinkedExpression() {
+    this.$().html(this.get('expression'));
+
+    this.get('sortedDataElements').forEach((dataElement) => {
+      let label = dataElement.get('label');
+      let URL = this.get('routing').generateURL('projects.show.data-elements.show', [
+        dataElement.get('project'),
+        dataElement
+      ]);
+      let link = `<a href="${URL}">${label}</a>`;
+
+      this.$().contents().filter(function() {
+        return this.nodeType === 3;
+      }).each(function() {
+        let $node = $(this);
+        let text = $node.text();
+        let rendered = text.replace(label, link);
+
+        $node.replaceWith(rendered);
+      });
+    });
+  }
+});
