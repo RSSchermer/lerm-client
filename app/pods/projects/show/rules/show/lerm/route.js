25c3c6512136fac5fcfe2c8ce46bbb0873a50740
R.S. Schermer <roland0507@gmail.com>
2016-03-31 11:01:47 +0200

    Initial attempt at anchor scrolling

--- app/pods/projects/show/rules/show/lerm/route.js
+++ app/pods/projects/show/rules/show/lerm/route.js
@@ -1,9 +1,32 @@
 import Ember from 'ember';
 
-const { Route } = Ember;
+const { Route, run, $, inject } = Ember;
+const { scheduleOnce } = run;
 
 export default Route.extend({
+  routing: inject.service('-routing'),
+
   model() {
     return this.modelFor('projects.show.rules.show');
+  },
+
+  actions: {
+    didTransition() {
+      console.log(this.get('queryParams.anchor'));
+      let url = this.get('router.url');
+
+      if (url.indexOf('#')) {
+        let anchor = url.split('#')[-1];
+        let $target = $(`#${anchor}`);
+
+        if ($target.length) {
+          scheduleOnce('afterRender', this, () => {
+            $('html, body').animate({
+              scrollTop: $target.offset().top - 10
+            });
+          });
+        }
+      }
+    }
   }
 });
