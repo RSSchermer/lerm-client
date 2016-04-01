ccb58615b94ea232e10577b23d21640d8e907441
R.S. Schermer <roland0507@gmail.com>
2016-04-01 13:50:50 +0200

    Add warning to step 4b when phrases are cleaned or discarded, but statements have not yet been adjusted
    Fixes #6

--- app/pods/projects/show/rules/show/lerm/step-4/controller.js
+++ app/pods/projects/show/rules/show/lerm/step-4/controller.js
@@ -7,7 +7,24 @@ const { alias } = computed;
 export default Controller.extend({
   rule: alias('model.rule'),
 
+  phrases: alias('rule.phrases'),
+
+  statements: alias('rule.statements'),
+
   isEditing: false,
+
+  displayCleanStatementWarning: computed(
+    'phrases.@each.cleanedText',
+    'phrases.@each.discarded',
+    'statements.@each.cleanedCondition',
+    'statements.@each.cleanedConsequence',
+    'statements.@each.discarded',
+    function() {
+      return this.get('phrases').any((p) => !!p.get('cleanedText') || p.get('discarded')) &&
+          !this.get('statements')
+            .any((s) => !!s.get('cleanedCondition') || !!s.get('cleanedConsequence') || s.get('discarded'));
+    }
+  ),
 
   savePhraseTask: task(function *(phrase) {
     try {
